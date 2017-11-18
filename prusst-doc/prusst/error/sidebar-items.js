eb9a33fb549be94628f819216cd355082828cc0b
Serge Barral <sbarral@quintescience.com>
2017-11-18 20:47:16 +0100

    Update to prusst 1.0 documentation

--- prusst-doc/prusst/error/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["Error","PRU subsystem error."]]});
--- prusst-doc/prusst/pubdef/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["Channel","A channel to which system interrupts can be mapped."],["Evtout","An event out."],["Host","A host to which channels can be mapped."],["Sysevt","A PRU-generated system event."]]});
